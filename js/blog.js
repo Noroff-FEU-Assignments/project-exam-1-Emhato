6a8632d4f97241f0953d5b5d51131bdae2e377d5
Emhato <71319235+Emhato@users.noreply.github.com>
2021-05-02 16:20:59 +0200

    posts

---
+++ js/blog.js
@@ -0,0 +1,81 @@
+// List of posts
+
+const url = "https://emmatonnessen.com/wp-json/wp/v2/posts/";
+
+const posts = document.querySelector(".posts");
+
+async function getPosts() {
+    try {
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+        console.log(data);
+
+        posts.innerHTML = "";
+
+        for (let i = 0; i < data.length; i++) {
+
+
+            if (i === 10) {
+                break;
+            }
+
+            // Add anchor tag and href here!
+            posts.innerHTML += `<div class="card">
+                                <h2>${data[i].title.rendered}</h2>
+                                <p>${data[i].excerpt.rendered}</p>
+                                </div>`;
+        }
+    } catch (error) {
+        console.log(error);
+        console.log("An error occurred");
+        posts.innerHTML = "An error has occured, sorry for the inconvinience this may cause!";
+    }
+
+}
+
+getPosts();
+
+
+// Show more button
+
+const showMore = document.querySelector("button");
+
+
+
+urlTwo = "https://emmatonnessen.com/wp-json/wp/v2/posts/?page=2"
+
+async function morePosts() {
+    try {
+        const response = await fetch(urlTwo);
+
+        const data = await response.json();
+
+        console.log(data);
+
+        // showMore.style.display = "none";
+
+
+        for (let i = 0; i < data.length; i++) {
+
+
+            if (i === 20) {
+                break;
+            }
+
+            // Add anchor tag and href here!
+            posts.innerHTML += `<div class="card">
+                                <h2>${data[i].title.rendered}</h2>
+                                <p>${data[i].excerpt.rendered}</p>
+                                </div>`;
+        }
+
+    } catch (error) {
+        console.log(error);
+        console.log("An error occurred");
+        posts.innerHTML = "An error has occured, sorry for the inconvinience this may cause!";
+    }
+}
+
+showMore.addEventListener("click", morePosts);

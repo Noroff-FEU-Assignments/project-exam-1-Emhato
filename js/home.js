1cd8ee67c428bfaf5fba6488c897a449c4585b6e
Emhato <71319235+Emhato@users.noreply.github.com>
2021-05-06 15:36:31 +0200

    carousel!!!

--- js/home.js
+++ js/home.js
@@ -66,86 +66,137 @@ getPosts().then(() => {
     const text2 = document.querySelector(".text2");
     const text3 = document.querySelector(".text3");
     const text4 = document.querySelector(".text4");
+
+    const circleIndicator = document.querySelector(".circle-indicator")
+
+    const circleFull = document.querySelector(".circle-full");
+    const circle2 = document.querySelector(".circle2");
+    const circle3 = document.querySelector(".circle3");
+    const circle4 = document.querySelector(".circle4");
     
     image1.addEventListener("click", function() {
+        //Image
         image2.classList.remove("selected");
         image3.classList.remove("selected");
         image4.classList.remove("selected");
 
         image1.classList.add("selected");
 
+        //header
         header1.style.display = "block";
 
         header2.style.display = "none";
         header3.style.display = "none";
         header4.style.display = "none";
 
+        //text
         text1.style.display = "block";
 
         text2.style.display = "none";
         text3.style.display = "none";
         text4.style.display = "none";
 
+
+
+        //circle indicator
+        circleIndicator.innerHTML = `<span class="circle circle-full"><i class="fas fa-circle"></i></span>
+                                    <span class="circle circle2"><i class="far fa-circle"></i></span>
+                                    <span class="circle circle3"><i class="far fa-circle"></i></span>
+                                    <span class="circle circle4"><i class="far fa-circle"></i></span>
+                                    `;
     });
 
     image2.addEventListener("click", function() {
+        //image
         image1.classList.remove("selected");
         image3.classList.remove("selected");
         image4.classList.remove("selected");
 
         image2.classList.add("selected");
 
+        //header
         header2.style.display = "block";
 
         header1.style.display = "none";
         header3.style.display = "none";
         header4.style.display = "none";
 
+        //text
         text2.style.display = "block";
 
         text1.style.display = "none";
         text3.style.display = "none";
         text4.style.display = "none";
+
+        //circle indicator
+        circleIndicator.innerHTML = `
+                                    <span class="circle circle2"><i class="far fa-circle"></i></span>
+                                    <span class="circle circle-full"><i class="fas fa-circle"></i></span>
+                                    <span class="circle circle3"><i class="far fa-circle"></i></span>
+                                    <span class="circle circle4"><i class="far fa-circle"></i></span>
+                                    `;
     });
 
     image3.addEventListener("click", function() {
+        //image
         image1.classList.remove("selected");
         image2.classList.remove("selected");
         image4.classList.remove("selected");
 
         image3.classList.add("selected");
 
+        //header
         header3.style.display = "block";
 
         header1.style.display = "none";
         header2.style.display = "none";
         header4.style.display = "none";
 
+        //text
         text3.style.display = "block";
 
         text1.style.display = "none";
         text2.style.display = "none";
         text4.style.display = "none";
+
+        // circle indicator
+        circleIndicator.innerHTML = `
+                                    <span class="circle circle2"><i class="far fa-circle"></i></span>
+                                    <span class="circle circle3"><i class="far fa-circle"></i></span>
+                                    <span class="circle circle-full"><i class="fas fa-circle"></i></span>
+                                    <span class="circle circle4"><i class="far fa-circle"></i></span>
+                                    `;
     });
 
     image4.addEventListener("click", function() {
+        //image
         image1.classList.remove("selected");
         image2.classList.remove("selected");
         image3.classList.remove("selected");
 
         image4.classList.add("selected");
 
+        //header
         header4.style.display = "block";
 
         header1.style.display = "none";
         header2.style.display = "none";
         header3.style.display = "none";
 
+        //text
         text4.style.display = "block";
 
         text1.style.display = "none";
         text2.style.display = "none";
         text3.style.display = "none";
+
+        //circle indicator
+        circleIndicator.innerHTML = `
+                                    <span class="circle circle2"><i class="far fa-circle"></i></span>
+                                    <span class="circle circle3"><i class="far fa-circle"></i></span>
+                                    <span class="circle circle4"><i class="far fa-circle"></i></span>
+                                    <span class="circle circle-full"><i class="fas fa-circle"></i></span>
+                                    `;
     });
 
 });

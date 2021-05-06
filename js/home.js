6201567b6f3d94f59e77d18ec7a0e500525f84e9
Emhato <71319235+Emhato@users.noreply.github.com>
2021-05-06 12:42:49 +0200

    carousel

--- js/home.js
+++ js/home.js
@@ -3,6 +3,8 @@
 const url = "https://emmatonnessen.com/wp-json/wp/v2/posts/";
 
 const images = document.querySelector(".carousel-images");
+const theHeader = document.querySelector(".carousel-header");
+const theText = document.querySelector(".carousel-text")
 
 async function getPosts() {
     try {
@@ -20,13 +22,25 @@ async function getPosts() {
             if (i === 1) {
                 break;
             }
+
+            theHeader.innerHTML += `<h2 class="header header2">${data[0].title.rendered}</h2>
+                                <h2 class="header header3">${data[1].title.rendered}</h2>
+                                <h2 class="header header4">${data[2].title.rendered}</h2>
+                                `;
+
             images.innerHTML += `<section class="carousel-section">
-                                    <figure class="carousel-image image1" style="background-image: url('images/sunrise.jpg')"></figure>
-                                    <figure class="carousel-image image2 selected" style="background-image: url('${data[0].better_featured_image.source_url}')"></figure>
+                                    <figure id="image1" class="carousel-image image1 selected" style="background-image: url('images/sunrise.jpg')"></figure>
+                                    <figure class="carousel-image image2" style="background-image: url('${data[0].better_featured_image.source_url}')"></figure>
                                     <figure class="carousel-image image3" style="background-image: url('${data[1].better_featured_image.source_url}')"></figure>
                                     <figure class="carousel-image image4" style="background-image: url('${data[2].better_featured_image.source_url}')"></figure>
                                 </section>
                                     `;
+            
+            theText.innerHTML += `<p class="text text2">${data[0].ec.rendered}</p>
+                                <p class="text text3">${data[1].content.rendered}</p>
+                                <p class="text text4">${data[2].content.rendered}</p>
+                                `;
+
         }
     } catch (error) {
         console.log(error);
@@ -36,28 +50,127 @@ async function getPosts() {
 
 }
 
-getPosts();
 
+getPosts().then(() => {
+    const image1 = document.querySelector(".image1");
+    const image2 = document.querySelector(".image2");
+    const image3 = document.querySelector(".image3");
+    const image4 = document.querySelector(".image4");
 
-const image1 = document.querySelector(".image1");
-const image2 = document.querySelector(".image2");
-const image3 = document.querySelector(".image3");
-const image4 = document.querySelector(".image4");
+    const header1 = document.querySelector(".header1");
+    const header2 = document.querySelector(".header2");
+    const header3 = document.querySelector(".header3");
+    const header4 = document.querySelector(".header4");
 
-function antiSelect() {
-    document.querySelector(".selected").classList.remove("selected");
-}
+    const text1 = document.querySelector(".text1");
+    const text2 = document.querySelector(".text2");
+    const text3 = document.querySelector(".text3");
+    const text4 = document.querySelector(".text4");
+    
+    image1.addEventListener("click", function() {
+        image2.classList.remove("selected");
+        image3.classList.remove("selected");
+        image4.classList.remove("selected");
 
-image1.addEventListener("click", function() {
-    classList.add("selected")
+        image1.classList.add("selected");
+
+        header1.style.display = "block";
+
+        header2.style.display = "none";
+        header3.style.display = "none";
+        header4.style.display = "none";
+
+        text1.style.display = "block";
+
+        text2.style.display = "none";
+        text3.style.display = "none";
+        text4.style.display = "none";
+
+    });
+
+    image2.addEventListener("click", function() {
+        image1.classList.remove("selected");
+        image3.classList.remove("selected");
+        image4.classList.remove("selected");
+
+        image2.classList.add("selected");
+
+        header2.style.display = "block";
+
+        header1.style.display = "none";
+        header3.style.display = "none";
+        header4.style.display = "none";
+
+        text2.style.display = "block";
+
+        text1.style.display = "none";
+        text3.style.display = "none";
+        text4.style.display = "none";
+    });
+
+    image3.addEventListener("click", function() {
+        image1.classList.remove("selected");
+        image2.classList.remove("selected");
+        image4.classList.remove("selected");
+
+        image3.classList.add("selected");
+
+        header3.style.display = "block";
+
+        header1.style.display = "none";
+        header2.style.display = "none";
+        header4.style.display = "none";
+
+        text3.style.display = "block";
+
+        text1.style.display = "none";
+        text2.style.display = "none";
+        text4.style.display = "none";
+    });
+
+    image4.addEventListener("click", function() {
+        image1.classList.remove("selected");
+        image2.classList.remove("selected");
+        image3.classList.remove("selected");
+
+        image4.classList.add("selected");
+
+        header4.style.display = "block";
+
+        header1.style.display = "none";
+        header2.style.display = "none";
+        header3.style.display = "none";
+
+        text4.style.display = "block";
+
+        text1.style.display = "none";
+        text2.style.display = "none";
+        text3.style.display = "none";
+    });
+
 });
 
 
 
-image1.addEventListener("click", antiSelect);
-image2.addEventListener("click", antiSelect);
-image3.addEventListener("click", antiSelect);
-image4.addEventListener("click", antiSelect);
+
+
+
+
+
+
+
+
+
+// images.innerHTML += `<section class="carousel-section">
+// <figure id="image1" class="carousel-image image1" style="background-image: url('images/sunrise.jpg')"></figure>
+// <figure class="carousel-image image2 selected" style="background-image: url('${data[0].better_featured_image.source_url}')"></figure>
+// <figure class="carousel-image image3" style="background-image: url('${data[1].better_featured_image.source_url}')"></figure>
+// <figure class="carousel-image image4" style="background-image: url('${data[2].better_featured_image.source_url}')"></figure>
+// </section>
+
+
+
+
 
 // <a href="blog-post.html?id=${data[i].id}" class="result">
 //                                         <div class="wrap">

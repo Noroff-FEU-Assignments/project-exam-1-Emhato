7b76a10daf3d26f1d633fe8dc323b784b1efa0f0
Emhato <71319235+Emhato@users.noreply.github.com>
2021-05-12 13:20:17 +0200

    enlarge

--- js/post.js
+++ js/post.js
@@ -29,10 +29,10 @@ async function getDetails() {
             title.innerHTML = `${data.title.rendered}`;
 
             post.innerHTML = `<div class="results">
-                                <h2>${data.title.rendered}</h2>
+                                <h2 class="remove1">${data.title.rendered}</h2>
                                 <div class="post-image"
                                     style="background-image: url('${data.better_featured_image.source_url}')" alt="${data.title.rendered}"></div>
-                                <p>${data.content.rendered}</p>
+                                <p class="remove2">${data.content.rendered}</p>
                             </div>`;
         
         // }
@@ -45,4 +45,48 @@ async function getDetails() {
 
 }
 
-getDetails();
+// getDetails();
+
+getDetails().then(() => {
+    const body = document.querySelector("body");
+    const image = document.querySelector(".post-image");
+    const main = document.querySelector("main")
+
+
+    image.addEventListener("click", function() {
+        body.classList.add("background");
+        image.classList.add("enlarged-image");
+        body.style.cursor = "pointer";
+        image.style.cursor = "default";
+        main.style.maxWidth = "1000px";
+    })
+
+    // body.addEventListener("click", function() {
+    //     if (body !== image) {
+    //         body.classList.remove("background");
+    //     }
+    // })
+
+    // remove2.addEventListener("click", function() {
+
+    //     body.classList.remove("background");
+    // })
+
+    document.addEventListener("click", function(event) {
+        if (!event.target.closest(".post-image")) {
+            body.classList.remove("background");
+            image.classList.remove("enlarged-image");
+            body.style.cursor = "default";
+            image.style.cursor = "pointer";
+            main.style.maxWidth = "800px";
+        }
+
+
+    })
+
+});
+
+
+// Resource
+// https://techstacker.com/close-modal-click-outside-vanilla-javascript/
+
